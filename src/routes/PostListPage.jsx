1efb2d10bbbb716c9b72e4ce9de783379a1cf66c
Aditya <atherva15@gmail.com>
2024-12-17 01:08:53 +0530

    added sidemenu and postlistpage

--- src/routes/PostListPage.jsx
+++ src/routes/PostListPage.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import PostList from "../components/PostList"
+import SideMenu from '../components/SideMenu'
+import { useState } from 'react';
 
 function PostListPage() {
+  const [open , setOpen] = useState(false);
+
   return (
-    <div>PostListPage</div>
+    <div className=''>
+      <h1 className='mb-8 text-2xl'>Developer</h1>
+      <button onClick={()=>setOpen((prev)=>!prev)} className='bg-blue-800 mb-4 text-white text-sm rounded-2xl px-4 py-2 md:hidden'>{ open ? "Close" : "Filter or Search"}</button>
+      <div className='flex gap-8'>
+        <div className=''>
+          <PostList/>
+        </div>
+        <div className=''>
+          <SideMenu/>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
