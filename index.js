60f31adcdeb8d9c31c612bacd766977658047970
Aditya <atherva15@gmail.com>
2024-12-20 01:30:30 +0530

    added models and controller for post CRUD started

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 import express from 'express';
-
+import dotenv from "dotenv";
+dotenv.config();
+import connectDb from './lib/connectDB.js';
+import userRouter from "./routes/user.route.js"
+import postRouter from "./routes/post.route.js"
+import commentRouter from "./routes/comment.route.js"
 const app = express();
 
 app.use(express.json());
 
+app.use("/users",userRouter);
+app.use("/posts",postRouter);
+app.use("/comments",commentRouter);
+
 
 app.listen(3000,()=>{
+    connectDb();
     console.log('Server is running on port 3000');
 })
